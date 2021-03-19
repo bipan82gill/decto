ebe6720b0fc2e78fce4bf882572e99735bb040f8
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-03-19 13:42:29 -0400

    adding about us

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
 
 function About() {
     return (
-        <div>
-            <h1>Hello from About us </h1>
-        </div>
+        <section className="about-info">
+            <h1>Hello from About us</h1>
+            <p>
+            It was founded with an idea to finish the gap between developers, experts, and businessmen so that customers can interact directly with the developers and experts to explain their business requirements. Along with the web development, we are the pioneers in web designing, e-commerce web development, SEO and digital marketing.
+            </p>
+            <p>
+            We focus on delivering high-quality services to different industries. This is the reason that more than 70% of our revenue is from our regular customers. With proven expertise to deliver the best products, our professionals are always ready to help businesses to attain a competitive edge.
+            </p>
+            <p>
+            Based in US, we have a team of talented professionals who provide services using the latest technologies. Our agile delivery methodology and experienced technologies have earned repeat engagements from clients. We are into the business expanding services in all across the world; with clients in different countries. It is focussed to maintain your online presence by implementing the latest strategies. Our experts understand the importance of your time so ensure that all tasks are accomplished on time. As online promotion and presence play a crucial role in business growth strategy, we propose an expressly design for you.
+            </p>
+            <p>
+            <b>Our Mission:</b> The online business solution market is growing very fast in the digital era, so business needs are also changing. It has a pool of professionals with creative minds who can build business-friendly products for you. Working on our mission to make the business reach their target customers, we offer fully operational app development, digital marketing, SEO services to turn your ideas into reality.
+                
+            </p>
+            <p>
+            <b>Our Vision:</b> We have the vision of maintaining a long term business relationship with customers so we ensure that our products should meet the business requirements of clients.
+                
+            </p>
+        </section>
+      
     )
 }
 
